9af3c5cc89cf834cf8c1bc8b83c83c228c677dad
Mounika <mounikagarlapati778@gmail.com>
2025-12-23 13:20:43 +0530

    data change

---
+++ src/main/java/com/example/githubintellijloanapp/LoanApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubintellijloanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationSystemApplication.class, args);
+    }
+
+}
